629b8eb6d0f5026254a48d4bcccae4072a6b3d95
keyzo8bp <keyzo8640@gmail.com>
2026-08-28 19:34:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -146,14 +146,4 @@ app.get("/", (req,res)=>{
 
 
 
-const PORT =
-process.env.PORT || 3000;
-
-
-app.listen(PORT,()=>{
-
- console.log(
-   "KEYZO API running on port "+PORT
- );
-
-});
+module.exports = app;
